e8ab055a9e01c2b8a46e4a1e46bb8bc8ea1f98b9
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-08-03 15:46:30 +0800

    penjelasan error yang jelas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,9 +79,21 @@
             );
             roastingResult = geminiResponse.data.roasting;
         } catch (error) {
-            console.error("Error fetching GitHub data", error);
-            roastingResult =
-                "Failed to fetch response, the possibility of the response being too harsh. please try again ...";
+            // console.error("Error fetching GitHub data", error);
+            if(axios.isAxiosError(error)){
+                //get response from error
+                var responseData = error.response?.data;
+                if(responseData.type == "AI"){
+                    roastingResult = "AI failed to return answer, the possibility of the response being too harsh or rude. please try again ...";
+                } else if(responseData.type == "Github"){
+                    roastingResult = "Error fetching GitHub data, probably has reached its limit. please try again later ...";
+                } else {
+                    roastingResult = "Failed to fetch response, error in server : "+error.message;
+                }
+            } else {
+                roastingResult = "Failed to fetch response, something wrong. please try again later ...";
+            }
+            
         }
         status = "done";
     }
