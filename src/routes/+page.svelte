6c83fc65d2f07ddadb3808e1a9bae87522ae0e30
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-08-03 12:42:22 +0800

    try request github api from client first

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,101 @@
 <script lang="ts">
-    import { PUBLIC_API_URL } from '$env/static/public'
+    import { PUBLIC_API_URL } from "$env/static/public";
     import axios from "axios";
 
-    let apiUrl = import.meta.env.VITE_API_URL ?? PUBLIC_API_URL ?? "http://localhost:3001";
-    let username: string = '';
-    let roastingResult: string = '';
-    let status : string = 'idle';
+    let apiUrl =
+        import.meta.env.VITE_API_URL ??
+        PUBLIC_API_URL ??
+        "http://localhost:3001";
+    let username: string = "";
+    let roastingResult: string = "";
+    let status: string = "idle";
 
     async function fetchGithubData() {
         if (!username) return;
-        status = 'loading';
+        status = "loading";
+        var datas = null;
         try {
-           
-            // Send data to Gemini AI for roasting
-            const geminiResponse = await axios.post<{ roasting: string }>(apiUrl+'/roast?username='+username);
-            roastingResult = geminiResponse.data.roasting;
-
+            var profileResponse = await axios.get(
+                `https://api.github.com/users/${username}`,
+            );
+            const repoResponse = await axios.get(
+                `https://api.github.com/users/${username}/repos?sort=updated`,
+            );
+            var readmeResponse = { status: 404 };
+            try {
+                readmeResponse = await axios.get(
+                    `https://raw.githubusercontent.com/${username}/${username}/main/README.md`,
+                );
+            } catch (error) {
+                try {
+                    readmeResponse = await axios.get(
+                        `https://raw.githubusercontent.com/${username}/${username}/master/README.md`,
+                    );
+                } catch (error) {}
+            }
+            datas = {
+                name: profileResponse.data.name,
+                bio: profileResponse.data.bio,
+                company: profileResponse.data.company,
+                location: profileResponse.data.location,
+                followers: profileResponse.data.followers,
+                following: profileResponse.data.following,
+                public_repos: profileResponse.data.public_repos,
+                repositories: repoResponse.data
+                    .map(
+                        (repo: {
+                            name: any;
+                            description: any;
+                            language: any;
+                            stargazers_count: any;
+                            open_issues_count: any;
+                            license: any;
+                            fork: any;
+                        }) => ({
+                            name: repo.name,
+                            description: repo.description,
+                            language: repo.language,
+                            stargazers_count: repo.stargazers_count,
+                            open_issues_count: repo.open_issues_count,
+                            license: repo.license,
+                            fork: repo.fork,
+                        }),
+                    )
+                    .slice(0, 50),
+            };
         } catch (error) {
-            console.error('Error fetching GitHub data', error);
-            roastingResult = 'Failed to fetch response, the possibility of the response being too harsh. please try again ...';
+            console.error("Error fetching GitHub data in client, try in server...", error);
         }
-        status = 'done';
+
+        try {
+            // Send data to Gemini AI for roasting
+            const geminiResponse = await axios.post<{ roasting: string }>(
+                apiUrl + "/roast?username=" + username,
+                {
+                    jsonData: JSON.stringify(datas),
+                },
+            );
+            roastingResult = geminiResponse.data.roasting;
+        } catch (error) {
+            console.error("Error fetching GitHub data", error);
+            roastingResult =
+                "Failed to fetch response, the possibility of the response being too harsh. please try again ...";
+        }
+        status = "done";
     }
 </script>
+
 <svelte:head>
     <title>GitHub Profile Roasting 🔥🔥🔥</title>
 </svelte:head>
 <main>
     <h1>GitHub Profile Roasting 🔥🔥🔥</h1>
-    
-    <input type="text" bind:value={username} placeholder="Enter GitHub username" />
+
+    <input
+        type="text"
+        bind:value={username}
+        placeholder="Enter GitHub username"
+    />
     <button on:click={fetchGithubData}>Submit</button>
 
     {#if status == "done"}
