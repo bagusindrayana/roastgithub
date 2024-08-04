3aae0372490f44445dc9cca7ea9112c369349cd8
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-08-04 14:09:54 +0800

    link trakteer untuk donasi dan setting untuk milih language dan model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,8 @@
         PUBLIC_API_URL ??
         "http://localhost:3001";
     let username: string = "";
+    let model: string = "gemini";
+    let language: string = "auto";
     let roastingResult: string = "";
     let status: string = "idle";
 
@@ -15,7 +17,7 @@
         if (!username || status == "loading") return;
         status = "loading";
         var datas = null;
-        var readmeResponse = { status: 404,data:null };
+        var readmeResponse = { status: 404, data: null };
         try {
             var profileResponse = await axios.get(
                 `https://api.github.com/users/${username}`,
@@ -23,7 +25,7 @@
             const repoResponse = await axios.get(
                 `https://api.github.com/users/${username}/repos?sort=updated`,
             );
-            
+
             try {
                 readmeResponse = await axios.get(
                     `https://raw.githubusercontent.com/${username}/${username}/main/README.md`,
@@ -65,14 +67,17 @@
                             open_issues_count: repo.open_issues_count,
                             license: repo.license,
                             fork: repo.fork,
-                            created_at : repo.created_at,
-                            updated_at : repo.updated_at
+                            created_at: repo.created_at,
+                            updated_at: repo.updated_at,
                         }),
                     )
                     .slice(0, 50),
             };
         } catch (error) {
-            console.error("Error fetching GitHub data in client, try in server...", error);
+            console.error(
+                "Error fetching GitHub data in client, try in server...",
+                error,
+            );
         }
 
         try {
@@ -81,26 +86,29 @@
                 apiUrl + "/roast?username=" + username,
                 {
                     jsonData: JSON.stringify(datas),
-                    README:readmeResponse.data
+                    README: readmeResponse.data,
+                    model:model,
+                    language:language
                 },
             );
             roastingResult = geminiResponse.data.roasting;
-        } catch (error : any) {
+        } catch (error: any) {
             console.error("Error : ", error);
-            if(axios.isAxiosError(error)){
+            if (axios.isAxiosError(error)) {
                 //get response from error
                 var responseData = error.response?.data;
-                if(responseData.type == "AI"){
-                    roastingResult = "AI failed to return answer, the possibility of the response being too harsh or get limit. please try again ...";
-                } else if(responseData.type == "Github"){
-                    roastingResult = "Error fetching GitHub data, "+responseData.error+". please try again later ...";
+                if(responseData != undefined && responseData.error){
+                    roastingResult =
+                        "Failed to fetch response, error in server : " +
+                        responseData.error;
                 } else {
-                    roastingResult = "Failed to fetch response, error in server : "+error.message;
+                    roastingResult =
+                        "Failed to fetch response, error in server : " +
+                        error?.message;
                 }
             } else {
                 roastingResult = `Failed to fetch response, something wrong : ${error?.message}. please try again later ...`;
             }
-            
         }
         status = "done";
     }
@@ -112,21 +120,61 @@
 <main>
     <h1>GitHub Profile Roasting 🔥🔥🔥</h1>
 
-    <input
-        type="text"
-        bind:value={username}
-        placeholder="Enter GitHub username"
-    />
-    <button on:click={fetchGithubData}>Submit</button>
+    <div>
+        <input
+            type="text"
+            bind:value={username}
+            placeholder="Enter GitHub username"
+        />
+        <button on:click={fetchGithubData}>Submit</button>
+    </div>
+    {#if status == "idle"}
+        <p>Enter a GitHub username to get started.</p>
+    {/if}
+    <br>
+    <details>
+        <summary>Setting</summary>
+        <div class="input-group">
+            <label for="language">Language</label>
+            <select bind:value={language} name="language" id="language">
+                <option value="auto">Auto</option>
+                <option value="english">English</option>
+                <option value="indonesia">Indonesia</option>
+            </select>
+        </div>
+        <div class="input-group">
+            <label for="model">AI Model</label>
+            <select bind:value={model} name="model" id="model">
+                <option value="gemini">Gemini AI</option>
+                <option value="llama">LLama</option>
+            </select>
+        </div>
+    </details>
 
     {#if status == "done"}
-        <h2>Roasting Result</h2>
+        <h2>Roasting For {username}</h2>
         <p>{roastingResult}</p>
     {:else if status == "loading"}
         <p>Loading...</p>
-    {:else if status == "idle"}
-        <p>Enter a GitHub username to get started.</p>
     {/if}
+    <div id="footer">
+        <center>
+            <p>Bantu Saya Bayar Tagihan GCP 😭</p>
+            <a href="https://trakteer.id/bagood" target="_blank"
+                ><img
+                    id="wse-buttons-preview"
+                    src="https://cdn.trakteer.id/images/embed/trbtn-red-1.png?date=18-11-2023"
+                    height="40"
+                    style="border: 0px; height: 40px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;"
+                    alt="Trakteer Saya"
+                    data-darkreader-inline-border-top=""
+                    data-darkreader-inline-border-right=""
+                    data-darkreader-inline-border-bottom=""
+                    data-darkreader-inline-border-left=""
+                /></a
+            >
+        </center>
+    </div>
 </main>
 
 <style>
@@ -153,5 +201,24 @@
         background: #f4f4f4;
         padding: 1em;
         overflow-x: auto;
+    }
+
+    #footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        padding: 16px;
+    }
+
+    .input-group {
+        margin-bottom: 16px;
+        display: flex;
+        gap: 6px;
+        flex-direction: column;
     }
 </style>
